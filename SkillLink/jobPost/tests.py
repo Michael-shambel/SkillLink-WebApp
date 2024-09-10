a25b186ef5e0807bab0051c5f349c4e8c750f2d9
michael-shambel <Michaelshambel10@gmail.com>
2024-09-10 15:15:26 -0700

    task jobpost

--- SkillLink/jobPost/tests.py
+++ SkillLink/jobPost/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
+from django.urls import timezone
+from users.models import CustomUser
+from .models import JobPost
 
-# Create your tests here.
+
+class JobPostModelTest(TestCase):
+    def setUp(self):
+        self.employer = CustomUser.objects.create_user(
+            email='testcase@company.com',
+            password='password',
+            is_employer=True
+        )
+    
+    def test_job_post_creation(self):
+        job_post = JobPost.objects.create(
+            title='gardener',
+            description='a person who can take care of the garden',
+            skills_required='gardening',
+            experience_required='2 years',
+            location='Bangalore',
+            posted_by=self.employer,
+            posted_on=timezone.now()
+        )
+        self.assertEqual(job_post.title, 'gardener')
+        self.assertEqual(job_post.description, 'a person who can take care of the garden')
+        self.assertEqual(job_post.skills_required, 'gardening')
+        self.assertEqual(job_post.experience_required, '2 years')
+        self.assertEqual(job_post.location, 'Bangalore')
+        self.assertEqual(job_post.posted_by, self.employer)
+        self.assertIsNotNone(job_post.posted_on)
