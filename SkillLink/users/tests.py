5c89c833045fcc59c148e2adb412a693db56596f
michael-shambel <Michaelshambel10@gmail.com>
2024-09-10 23:48:42 +0300

    task unittest

--- SkillLink/users/tests.py
+++ SkillLink/users/tests.py
@@ -3,6 +3,8 @@ from django.contrib.auth import get_user_model
 from .models import JobSeekerProfile, EmployerProfile
 from .forms import UserRegistrationForm, JobSeekerProfileForm, EmployerProfileForm
 from .models import CustomUser
+from django.urls import reverse, resolve
+from .views import register, select_by_user, job_seeker_profile, employer_profile
 
 class CustomUserTests(TestCase):
     def setUp(self):
@@ -43,11 +45,12 @@ class FormTests(TestCase):
     def test_user_registration_form_valid(self):
         form_data = {
             'email': 'testuser@examole.com',
-            'password1': 'password123',
-            'password2': 'password123',
+            'password1': 'password123zxzzx',
+            'password2': 'password123zxzzx',
         }
         form = UserRegistrationForm(data=form_data)
-        self.assertTrue(form.is_valid())
+        print(form.errors)
+        self.assertTrue(form.is_valid(), True)
     
     def test_user_registration_form_invalid_email(self):
         form_data = {
@@ -65,12 +68,13 @@ class FormTests(TestCase):
             'last_name': 'sham',
             'phone_number': '0987654321',
             'skills': 'Plumber',
-            'experiance': '5 years',
+            'experience': '5 years',
         }
         form = JobSeekerProfileForm(data=form_data)
-        self.assertEqual(form.is_valid())
+        print(form.errors)
+        self.assertEqual(form.is_valid(), True)
     
-    def test_jobseeker_profile_form_missing_first_name(Self):
+    def test_jobseeker_profile_form_missing_first_name(self):
         form_data = {
             'last_name': 'sham',
             'phone_number': '0987654321',
@@ -78,8 +82,8 @@ class FormTests(TestCase):
             'experience': '5 years'
         }
         form = JobSeekerProfileForm(data=form_data)
-        self.assertFalse(form.is_valid())
-        self.asserIn('first_name', form.errors)
+        self.assertFalse(form.is_valid(), False)
+        self.assertIn('first_name', form.errors)
     
     def test_employer_profile_form_valid(self):
         form_data = {
@@ -100,3 +104,21 @@ class FormTests(TestCase):
         form = EmployerProfileForm(data=form_data)
         self.assertFalse(form.is_valid())
         self.assertIn('phone_number', form.errors)
+
+
+class UrlTests(TestCase):
+    def test_register_url_resolves(self):
+        url = reverse('register')
+        self.assertEqual(resolve(url).func, register)
+    
+    def test_select_user_type_url_resolves(self):
+        url = reverse('select_by_user')
+        self.assertEqual(resolve(url).func, select_by_user)
+    
+    def test_job_seeker_profile_url_resolves(self):
+        url = reverse('job_seeker_profile')
+        self.assertEqual(resolve(url).func, job_seeker_profile)
+    
+    def test_employer_profile_url_resolves(self):
+        url = reverse('employer_profile')
+        self.assertEqual(resolve(url).func, employer_profile)
