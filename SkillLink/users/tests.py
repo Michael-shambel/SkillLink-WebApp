e95e5e1c0661f8345fc05c9314c7812d8d3e95c0
michael-shambel <Michaelshambel10@gmail.com>
2024-09-10 05:23:26 -0700

    task test

--- SkillLink/users/tests.py
+++ SkillLink/users/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from django.contrib.auth import get_user_model
 from .models import JobSeekerProfile, EmployerProfile
 from .forms import UserRegistrationForm, JobSeekerProfileForm, EmployerProfileForm
+from .models import CustomUser
 
 class CustomUserTests(TestCase):
     def setUp(self):
@@ -36,3 +37,66 @@ class CustomUserTests(TestCase):
         self.assertEqual(profile.first_name, 'kerem')
         self.assertEqual(profile.last_name, 'darwash')
         self.assertEqual(profile.phone_number, '0987654321')
+
+class FormTests(TestCase):
+
+    def test_user_registration_form_valid(self):
+        form_data = {
+            'email': 'testuser@examole.com',
+            'password1': 'password123',
+            'password2': 'password123',
+        }
+        form = UserRegistrationForm(data=form_data)
+        self.assertTrue(form.is_valid())
+    
+    def test_user_registration_form_invalid_email(self):
+        form_data = {
+            'email': 'invalid-email',
+            'password1': 'password123',
+            'password2': 'password123',
+        }
+        form = UserRegistrationForm(data=form_data)
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['email'], ['Enter a valid email address.'])
+    
+    def test_jobseeker_profile_form_valid(self):
+        form_data = {
+            'first_name': 'miki',
+            'last_name': 'sham',
+            'phone_number': '0987654321',
+            'skills': 'Plumber',
+            'experiance': '5 years',
+        }
+        form = JobSeekerProfileForm(data=form_data)
+        self.assertEqual(form.is_valid())
+    
+    def test_jobseeker_profile_form_missing_first_name(Self):
+        form_data = {
+            'last_name': 'sham',
+            'phone_number': '0987654321',
+            'skills': 'plumber',
+            'experience': '5 years'
+        }
+        form = JobSeekerProfileForm(data=form_data)
+        self.assertFalse(form.is_valid())
+        self.asserIn('first_name', form.errors)
+    
+    def test_employer_profile_form_valid(self):
+        form_data = {
+            'first_name': 'Jane',
+            'last_name': 'smith',
+            'phone_number': '0987654321',
+            'location': 'New York'
+        }
+        form = EmployerProfileForm(data=form_data)
+        self.assertTrue(form.is_valid())
+    
+    def test_employer_profile_form_missing_phone_number(self):
+        form_data = {
+            'first_name': 'Jane',
+            'last_name': 'Smith',
+            'location': 'New York',
+        }
+        form = EmployerProfileForm(data=form_data)
+        self.assertFalse(form.is_valid())
+        self.assertIn('phone_number', form.errors)
